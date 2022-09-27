47991882a9e1103c471a4484a8b7194b1f556147
CosminTotal <cosmin.gostin@totalsoft.ro>
2022-09-27 15:14:43 +0300

    s

--- src/features/changePassword/schema.js
+++ src/features/changePassword/schema.js
@@ -4,8 +4,18 @@ const changePasswordDefs = gql`
   type Password {
     password: String
   }
+
+  input UpdatePassword {
+    newPassword: String
+    angajatId: Int
+  }
+
   extend type Query {
     Password(id: Int): String
+  }
+
+  extend type Mutation {
+    changePassword(input: UpdatePassword): Boolean
   }
 `
 
